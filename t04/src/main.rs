4583cd57127b3d4e17c9cc568b5316604c931528
aoaochan <josejbeck99@yahoo.com>
2025-05-31 14:14:58 +0900

    t05

--- t04/src/main.rs
+++ t04/src/main.rs
@@ -122,56 +122,56 @@ fn main() {
     //     println!("The maximum is configured to br {max}");
     // }
     
-    #[derive(Debug)]
-    enum UsState {
-        Alabama,
-        Alaska
-    }
-    enum Coin {
-        Penny,
-        Nickel,
-        Dime,
-        Quarter(UsState)
-    }
-    impl UsState {
-        fn existed_in(&self, year: u16) -> bool {
-            match self {
-                UsState::Alabama => year >= 1819,
-                UsState::Alaska => year >= 1959,
-                _ => false
-            }
-        }
-    }
-    fn describe_state_quarter(coin: Coin) -> Option<String> {
-        if let Coin::Quarter(state) = coin {
-            if state.existed_in(1900) {
-                Some(format!("{state:?} is pretty old, for America!"))
-            } else {
-                Some(format!("{state:?} is relatively new."))
-            }
-        } else {
-            None
-        }
-    }
-    fn _describe_state_quarter(coin: Coin) -> Option<String> {
-        // let state = if let Coin::Quarter(state) = coin {
-        //     state
-        // } else {
-        //     return None;
-        // };
-        // let Coin::Quarter(state) = coin else { return None; };
-        // let state = if let Coin::Quarter(state) = coin {
-        //     state
-        // } else {
-        //     return None;
-        // };
-        let Coin::Quarter(state) = coin else { return None; };
-        if state.existed_in(1900) {
-            Some(format!("{state:?} is pretty old, for America!"))
-        } else {
-            Some(format!("{state:?} is relatively new."))
-        }
-    }
+    // #[derive(Debug)]
+    // enum UsState {
+    //     Alabama,
+    //     Alaska
+    // }
+    // enum Coin {
+    //     Penny,
+    //     Nickel,
+    //     Dime,
+    //     Quarter(UsState)
+    // }
+    // impl UsState {
+    //     fn existed_in(&self, year: u16) -> bool {
+    //         match self {
+    //             UsState::Alabama => year >= 1819,
+    //             UsState::Alaska => year >= 1959,
+    //             _ => false
+    //         }
+    //     }
+    // }
+    // fn describe_state_quarter(coin: Coin) -> Option<String> {
+    //     if let Coin::Quarter(state) = coin {
+    //         if state.existed_in(1900) {
+    //             Some(format!("{state:?} is pretty old, for America!"))
+    //         } else {
+    //             Some(format!("{state:?} is relatively new."))
+    //         }
+    //     } else {
+    //         None
+    //     }
+    // }
+    // fn _describe_state_quarter(coin: Coin) -> Option<String> {
+    //     // let state = if let Coin::Quarter(state) = coin {
+    //     //     state
+    //     // } else {
+    //     //     return None;
+    //     // };
+    //     // let Coin::Quarter(state) = coin else { return None; };
+    //     // let state = if let Coin::Quarter(state) = coin {
+    //     //     state
+    //     // } else {
+    //     //     return None;
+    //     // };
+    //     let Coin::Quarter(state) = coin else { return None; };
+    //     if state.existed_in(1900) {
+    //         Some(format!("{state:?} is pretty old, for America!"))
+    //     } else {
+    //         Some(format!("{state:?} is relatively new."))
+    //     }
+    // }
     // let mut count = 0;
     // let coin = Coin::Penny;
     // match coin {
---
+++ t05/README.md
@@ -0,0 +1,4 @@
+# t05
+
+## Credit
+- https://doc.rust-lang.org/book/ch07-00-managing-growing-projects-with-packages-crates-and-modules.html
