03ece5e49f0507cb6562482c6ae06883564f891b
aoaochan <josejbeck99@yahoo.com>
2025-06-13 16:31:24 +0900

    t10-building-a-cli-program

--- t10-building-a-cli-program/minigrep/src/main.rs
+++ t10-building-a-cli-program/minigrep/src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::process;
+
+use minigrep::{Config, run};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.file_path);
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
