a55545fedade5f3c224d40790ff89db10e09e84c
aoaochan <josejbeck99@yahoo.com>
2025-06-14 21:52:00 +0900

    t11 (3)

--- t11/closures1/src/main.rs
+++ t11/closures1/src/main.rs
@@ -1,9 +1,44 @@
 fn main() {
-    let list = vec![1, 2, 3];
-    println!("Before defining closure: {list:?}");
-    std::thread::spawn(move || println!("From thread: {list:?}"))
-        .join()
-        .unwrap();
+    #[derive(Debug)]
+    struct Rectangle { width: u32, height: u32 }
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+    // list.sort_by_key(|r| r.width);
+    // println!("{list:#?}");
+    // let mut sort_op = vec![];
+    // let v = String::from("closure called");
+    // list.sort_by_key(|r| {
+    //     sort_op.push(v);
+    //     r.width
+    // });
+    // println!("{list:#?}");
+    let mut cnt_sort_op = 0;
+    list.sort_by_key(|r| {
+        cnt_sort_op += 1;
+        r.width
+    });
+    println!("{list:#?}, sorted in {cnt_sort_op} operations");
+
+    // impl<T> Option<T> {
+    //     pub fn unwrap_or_else<F>(self, f: F) -> T
+    //     where 
+    //         F: FnOnce() -> T
+    //     {
+    //         match self T {
+    //             Some(x) => x,
+    //             None => f()
+    //         }
+    //     }
+    // }
+
+    // let list = vec![1, 2, 3];
+    // println!("Before defining closure: {list:?}");
+    // std::thread::spawn(move || println!("From thread: {list:?}"))
+    //     .join()
+    //     .unwrap();
 
     // let mut list = vec![1, 2, 3];
     // println!("Before defining closure: {list:?}");
