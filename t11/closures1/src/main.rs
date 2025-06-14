a5e1f24c458d8f4980981602952c086bddc64ef8
aoaochan <josejbeck99@yahoo.com>
2025-06-14 21:35:43 +0900

    t11 (2)

---
+++ t11/closures1/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+    std::thread::spawn(move || println!("From thread: {list:?}"))
+        .join()
+        .unwrap();
+
+    // let mut list = vec![1, 2, 3];
+    // println!("Before defining closure: {list:?}");
+
+    // let mut borrows_mutably = || list.push(7);
+
+    // borrows_mutably();
+    // println!("After calling closure: {list:?}");
+
+    // let list = vec![1, 2, 3];
+    // println!("Before defining closure: {list:?}");
+
+    // let only_borrows = || println!("From closure: {list:?}");
+
+    // println!("Before calling closure: {list:?}");
+    // only_borrows();
+    // println!("After calling closure: {list:?}");
+}
