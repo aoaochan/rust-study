09e3f2876e74cf61003993b2dcb01dd762f23a06
aoaochan <josejbeck99@yahoo.com>
2025-06-14 22:22:42 +0900

    t11 (5)

--- t10-building-a-cli-program/minigrep/src/lib.rs
+++ t10-building-a-cli-program/minigrep/src/lib.rs
@@ -9,12 +9,27 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 { return Err("not enough arguments"); }
+    // pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(
+        mut args: impl Iterator<Item = String>
+    ) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a query string")
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Did not get a file path")
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        // if args.len() < 3 { return Err("not enough arguments"); }
+
+        // let query = args[1].clone();
+        // let file_path = args[2].clone();
+        // let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config { query, file_path, ignore_case })
     }
@@ -34,15 +49,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = vec![];
-    
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);        
-        }
-    }
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 
-    result
+    // let mut result = vec![];
+    
+    // for line in contents.lines() {
+    //     if line.contains(query) { result.push(line); } 
+    // }
+
+    // result
 }
 
 pub fn search_case_insensitive<'a>(
--- t10-building-a-cli-program/minigrep/src/main.rs
+++ t10-building-a-cli-program/minigrep/src/main.rs
@@ -4,11 +4,16 @@ use std::process;
 use minigrep::{Config, run};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
     
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
+    // let config = Config::build(&args).unwrap_or_else(|err| {
+    //     eprintln!("Problem parsing arguments: {err}");
+    //     process::exit(1);
+    // });
+
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+            eprintln!("Problem parsing arguments: {err}");
+            process::exit(1);
     });
 
     // println!("Searching for {}", config.query);
