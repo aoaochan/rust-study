1cfa3cd7cd5d04ccbc9daa987c716833b61c924c
aoaochan <josejbeck99@yahoo.com>
2025-06-15 11:03:40 +0900

    t12 (1)

---
+++ t12-smart-pointer/box_generic/src/lib.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+mod test {
+    // enum List { Cons(i32, List), Nil }
+    enum List { Cons(i32, Box<List>), Nil }
+    #[test]
+    fn foo() {
+        // let list = Cons(1, Cons(2, Cons(3, Nil)));
+        let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    }
+
+    // fn foo() {
+    //     let b = Box::new(5);
+    //     println!("b = {b}");
+    // }
+}
